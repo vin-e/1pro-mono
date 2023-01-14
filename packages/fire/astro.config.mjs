885fa8a28d1dbc5ab0c96c6baf894f5bdd23f05c
Vincent Chavez <vincent@vin-e.com>
2023-01-13 16:56:27 -0800

    not done yet

--- packages/fire/astro.config.mjs
+++ packages/fire/astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
+import autoprefixer from 'autoprefixer';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   trailingSlash: 'always',
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer({}) // add options if needed
+        ],
+      }
+    }
+  }
 });
