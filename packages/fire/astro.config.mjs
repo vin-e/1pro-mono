43df03f41a10854ca09b0e78603ba93af645864f
Vincent Chavez <vincent@vin-e.com>
2023-04-08 12:27:49 -0700

    restarted updates

--- packages/fire/astro.config.mjs
+++ packages/fire/astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     css: {
       postcss: {
         plugins: [
-          autoprefixer({}) // add options if needed
+          autoprefixer({})
         ],
       }
     }
